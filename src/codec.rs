1895c64de16d5a788bd6a54dbaf47a6350c4ce7b
Marcio Giaxa <i@mgxm.me>
2018-02-09 14:21:09 -0200

    Added Integer encoding and decoding

--- src/codec.rs
+++ src/codec.rs
@@ -34,6 +34,26 @@ impl Delta {
     }
 }
 
+pub struct Integer;
+
+impl Integer {
+    fn decode(values: &[i32], factor: i32) -> Vec<f32> {
+        let result: Vec<f32> = values
+            .iter()
+            .map(|x| *x as f32 / factor as f32)
+            .collect();
+        result
+    }
+
+    fn encode(values: &[f32], factor: i32) -> Vec<i32> {
+        let result: Vec<i32> = values
+            .iter()
+            .map(|x| (x * factor as f32) as i32)
+            .collect();
+        result
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -42,7 +62,7 @@ mod tests {
     fn it_decode_run_length() {
         let encoded = [1, 4, 2, 1, 1, 4];
         let decoded = RunLength::decode(&encoded);
-        assert_eq!(vec![1,1,1,1,2,1,1,1,1], decoded);
+        assert_eq!(vec![1, 1, 1, 1, 2, 1, 1, 1, 1], decoded);
     }
 
     #[test]
@@ -50,6 +70,22 @@ mod tests {
         let data = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 5];
         let expected = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 13, 14, 15, 20];
         let actual = Delta::decode(&data);
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn test_codec_integer_decoding() {
+        let data = [100, 100, 100, 100, 50, 50];
+        let expected = vec![1.00, 1.00, 1.00, 1.00, 0.50, 0.50];
+        let actual = Integer::decode(&data, 100);
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn test_codec_integer_encoding() {
+        let data = [1.00, 1.00, 1.00, 1.00, 0.50, 0.50];
+        let expected = vec![100, 100, 100, 100, 50, 50];
+        let actual = Integer::encode(&data, 100);
         assert_eq!(expected, actual);
     }
 }
